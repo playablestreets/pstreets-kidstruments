3a827ccc92aabe0ab5fc78baf11126e1a0ef6e26
Bob Jarvis <bob@zeal.co>
2020-07-07 15:30:02 +0930

    dynamic image loading

--- script.js
+++ script.js
@@ -17,7 +17,6 @@
 // alpha, normx, normy, duration;
 // manually start audioContext.  Perhaps use an instructional splash screen...
 
-
 'use strict';
 // let synths = [];
 let hasBegun = false;
@@ -41,38 +40,47 @@ var instrumentImage;
 var maskImage;
 var currentColor;
 var currentHue;
-// var hueUI;
 var normMouseX;
 var normMouseY;
 var maskImageScale = 16; //
 var drawScale = 0.2;
 var offset = [ 0, 0 ];
 
-function preload() {
-	instrumentImage = loadImage('assets/Daphne_7_3916.png');
-	maskImage = loadImage('assets/Daphne_7_3916-MASK.png');
+function windowResized() {
+	if (instrumentImage.isLoaded && maskImage.isLoaded) {
+		let uiOffset = 35;
+
+		drawScale = windowWidth / instrumentImage.width;
+		if (windowWidth > windowHeight) {
+			drawScale = windowHeight / instrumentImage.height;
+		}
+
+		let xOffset = windowWidth / 2;
+		xOffset -= instrumentImage.width * drawScale / 2;
+		let yOffset = windowHeight / 2;
+		yOffset -= instrumentImage.height * drawScale / 2 + uiOffset;
+
+		offset = {
+			x: xOffset,
+			y: yOffset
+		};
+
+		resizeCanvas(windowWidth, windowHeight);
+	}
 }
 
-function windowResized() {
-
-	let uiOffset = 35;
-
-	drawScale = windowWidth/instrumentImage.width;
-	if(windowWidth>windowHeight){
-		drawScale = windowHeight/(instrumentImage.height);
-	}
-
-	let xOffset = windowWidth / 2;
-	xOffset -= instrumentImage.width * drawScale / 2;
-	let yOffset = windowHeight / 2;
-	yOffset -= instrumentImage.height * drawScale / 2 + uiOffset;
-
-	offset = {
-		x: xOffset,
-		y: yOffset
-	};
-
-	resizeCanvas(windowWidth, windowHeight);
+function loadInstrument() {
+	instrumentImage = loadImage('assets/Daphne_7_3916.png', () => {
+		instrumentImage.isLoaded = true;
+		windowResized();
+	});
+	maskImage = loadImage('assets/Daphne_7_3916-MASK.png', () => {
+		maskImage.resize(maskImage.width / maskImageScale, maskImage.height / maskImageScale);
+		maskImage.loadPixels();
+		maskImage.isLoaded = true;
+		windowResized();
+	});
+	document.getElementById('info').innerHTML = 'INSTRUMENT by \n NAME';
 }
 
 ///SETUP
@@ -80,32 +88,24 @@ function setup() {
 	console.log('hi');
 	pixelDensity(1);
 
-	//set up sounds
+	loadInstrument();
 
-	let synth = new Tone.Synth(
-		B[2]
-	).toMaster();
+	//set up sounds
+	let synth = new Tone.Synth(B[2]).toMaster();
 	let sound = new Sound(synth);
 	sounds.push(sound);
 
-	synth = new Tone.Synth(
-		B[4]
-	).toMaster();
+	synth = new Tone.Synth(B[4]).toMaster();
 	sound = new Sound(synth);
 	sounds.push(sound);
 
-	synth = new Tone.Synth(
-		V[3]
-	).toMaster();
+	synth = new Tone.Synth(V[3]).toMaster();
 	sound = new Sound(synth);
 	sounds.push(sound);
 
 	divX = width / notes.length;
 	divY = height / octaves.length;
 
-	//debug gui
-	// hueUI = createElement('h2', 'nothin has happened');
-	document.getElementById("info").innerHTML = "hello";
 	currentColor = color(255);
 
 	// Create the canvas
@@ -113,15 +113,9 @@ function setup() {
 	canvas.position(0, 0);
 	canvas.style('z-index', -1);
 
-	//set up mask image
-	maskImage.resize(maskImage.width / maskImageScale, maskImage.height / maskImageScale);
-	maskImage.loadPixels();
-
 	//resize window to init
 	windowResized();
 }
-
-
 
 ///DRAW
 function draw() {
@@ -130,23 +124,25 @@ function draw() {
 
 	fill(255);
 
-	image(instrumentImage, offset.x, offset.y, instrumentImage.width * drawScale, instrumentImage.height * drawScale);
-	if (drawMask) {
-		image(
-			maskImage,
-			offset.x,
-			offset.y,
-			maskImage.width * maskImageScale * drawScale,
-			maskImage.height * maskImageScale * drawScale
-		);
-	}
+	if (instrumentImage.isLoaded && maskImage.isLoaded) {
+		image(instrumentImage, offset.x, offset.y, instrumentImage.width * drawScale, instrumentImage.height * drawScale);
+		if (drawMask) {
+			image(
+				maskImage,
+				offset.x,
+				offset.y,
+				maskImage.width * maskImageScale * drawScale,
+				maskImage.height * maskImageScale * drawScale
+			);
+		}
 
-	if (mouseX > 10 && mouseX < width - 10 && (mouseY > 10 && mouseY < height - 10)) {
-		let ellipseWidth = mouseIsPressed ? 70 : 0;
-		stroke(240, 100);
-		strokeWeight(5);
-		fill(currentColor);
-		ellipse(mouseX, mouseY, ellipseWidth);
+		if (mouseX > 10 && mouseX < width - 10 && (mouseY > 10 && mouseY < height - 10)) {
+			let ellipseWidth = mouseIsPressed ? 70 : 0;
+			stroke(240, 100);
+			strokeWeight(5);
+			fill(currentColor);
+			ellipse(mouseX, mouseY, ellipseWidth);
+		}
 	}
 }
 
@@ -167,8 +163,6 @@ function update() {
 		else {
 			currentHue = -1;
 		}
-		document.getElementById("info").innerHTML = currentHue;
-		// hueUI.elt.style.color = '#999999';
 	}
 	else {
 		currentColor = color(255);
@@ -178,30 +172,22 @@ function update() {
 	if (isPressed && currentHue >= 0) {
 		activeSynth = parseInt(map(currentHue, 0, 360, 0, sounds.length));
 
-		// let note = map(getNormMouse().x, 0, 1, 400, 2000);
-		// console.log('note: ' + note +  ', activeSynth: ' + activeSynth);
-
 		let i = 0;
-		sounds.forEach(sound => {
-			if(activeSynth != i)
-				sound.stopNotes();
-			else
-				sound.playNote();
+		sounds.forEach((sound) => {
+			if (activeSynth != i) sound.stopNotes();
+			else sound.playNote();
 			i++;
 		});
-
-		
 	}
-	// release all notes
 	else {
-		sounds.forEach(sound => {
-				sound.stopNotes();
+		// release all notes
+		sounds.forEach((sound) => {
+			sound.stopNotes();
 		});
 	}
 }
 
-
-function fillBg(){
+function fillBg() {
 	if (isPressed && currentHue >= 0) {
 		colorMode(HSB);
 		background(currentHue, 50, 100, 0.2);
@@ -210,13 +196,11 @@ function fillBg(){
 	else {
 		background(255, 100);
 	}
-
 }
 
 ///ONTOUCH
 function go() {
-
-	if(!hasBegun){
+	if (!hasBegun) {
 		console.log('starting tone.js');
 		Tone.start();
 		hasBegun = true;
